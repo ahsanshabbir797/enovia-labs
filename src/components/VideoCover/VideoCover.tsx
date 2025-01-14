98479462fdebd8eae301ebb150e0436627f704b8
Malik Sharoz Tariq <maliksharoz70@outlook.com>
2025-01-15 04:23:50 +0500

    Create video cover component and implement it on home page

---
+++ src/components/VideoCover/VideoCover.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./VideoCover.module.css";
+
+interface VideoCoverProps {
+  source: string;
+  header?: string;
+  description?: string;
+}
+
+export default function VideoCover({
+  source,
+  header,
+  description,
+}: VideoCoverProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <section className={styles.videoContainer} aria-label="Video cover section">
+      <video
+        ref={videoRef}
+        loop
+        autoPlay={isVisible}
+        muted
+        className={styles.bgVideo}
+      >
+        {isVisible && <source src={source} type="video/mp4" />}
+        {isVisible && (
+          <source src={source.replace(".mp4", ".webm")} type="video/webm" />
+        )}
+        Your browser does not support the video tag.
+      </video>
+      <header className={styles.overlayContent}>
+        {header && <h1 className={styles.header}>{header}</h1>}
+        {description && <p className={styles.description}>{description}</p>}
+      </header>
+    </section>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,17 @@
+import VideoCover from "../../components/VideoCover/VideoCover";
+import softwareDevelopment from "../../assets/software-development.mp4";
+import styles from "./Home.module.css";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.homePage}>
+      <div className={styles.videoContainer}>
+        <VideoCover
+          source={softwareDevelopment}
+          header="Empowering Your Vision with Cutting-Edge Software Solutions"
+          description="We help companies redefine the future through technology. Our team of experts will help you build the right software solutions for your business."
+        />
+      </div>
+    </div>
+  );
 }
