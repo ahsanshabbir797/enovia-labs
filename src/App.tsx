61ec9b7e2c89cc26706162d84c5feed30d58f03b
Malik Sharoz Tariq <maliksharoz70@outlook.com>
2025-01-16 16:03:10 +0500

    Implement footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import AboutUs from "./pages/About-Us/AboutUs";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer.tsx";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-          </Routes>
-        </main>
+        <div className="appContainer">
+          <Navbar />
+          <main className="mainContent">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about-us" element={<AboutUs />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </BrowserRouter>
     </div>
   );
