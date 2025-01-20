fb534c3ef1950a5dc170b427fbff4a4a9cb187b1
Malik Sharoz Tariq <maliksharoz70@outlook.com>
2025-01-20 16:27:14 +0500

    Implement asks by the client

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,6 @@ import VideoCover from "../../components/VideoCover/VideoCover";
 import softwareDevelopment from "../../assets/software-development.mp4";
 import styles from "./Home.module.css";
 import OurServices from "../../components/Our-Services/Our-Services";
-import OurIndustries from "../../components/Our-Industries/Our-Industries";
 import OurInsights from "../../components/Our-Insights/OurInsights";
 import OurAchievements from "../../components/Our-Achievements/Our-Achievements";
 
@@ -16,7 +15,6 @@ export default function Home() {
           description="We help companies redefine the future through technology. Our team of experts will help you build the right software solutions for your business."
         />
         <OurServices />
-        <OurIndustries />
         <OurInsights />
         <OurAchievements />
       </div>
