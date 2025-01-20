a70a6ac0eddca681dd2d1b9c06503720ae6896fa
ahsanshabbir797 <ahsanshabbir797@gmail.com>
2025-01-20 23:31:04 +0500

    made navbar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,22 @@
 import "./Navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 type MenuItem = {
   option: string;
   isOpen: boolean;
-  routeLink:string;
+  routeLink: string;
 };
 
 const Navbar = () => {
   const [menuItems, setMenuItems] = useState<MenuItem[]>([
-    { option: "Home",routeLink:"/", isOpen: false },
-    { option: "About Us",routeLink:"/about-us", isOpen: false },
-    { option: "Services",routeLink:"/services", isOpen: false },
-    { option: "Projects",routeLink:"/projects", isOpen: false },
-    { option: "Contact Us",routeLink:"/contact-us", isOpen: false },
+    { option: "Home", routeLink: "/", isOpen: false },
+    { option: "About Us", routeLink: "/about-us", isOpen: false },
+    { option: "Services", routeLink: "/services", isOpen: false },
+    { option: "Projects", routeLink: "/projects", isOpen: false },
+    { option: "Contact Us", routeLink: "/contact-us", isOpen: false },
   ]);
 
   const [rightMenu, setRightMenu] = useState({
@@ -24,10 +24,16 @@ const Navbar = () => {
     isOpen: false,
   });
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const handleToggle = (index: number) => {
     const updatedItems = [...menuItems];
     updatedItems[index].isOpen = !updatedItems[index].isOpen;
     setMenuItems([...updatedItems]);
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
@@ -36,31 +42,42 @@ const Navbar = () => {
         <span className="logo-text-enovia">Enovia</span>
         <span className="logo-text-labs">Labs</span>
       </div>
-      <div>
+
+      <div className="mobile-menu-button" onClick={toggleMobileMenu}>
+        <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
+      </div>
+
+      <div className={`nav-content ${isMobileMenuOpen ? 'show' : ''}`}>
         <ul className="navbar-items">
           {menuItems.map((item, index) => (
             <li key={index} onClick={() => handleToggle(index)}>
-              <Link className="link-text" to={item.routeLink}>{item.option}</Link>
+              <Link className="link-text" to={item.routeLink}>
+                {item.option}
+              </Link>
             </li>
           ))}
         </ul>
-      </div>
-      <div className="buttons-container">
-        <button className="nav-button"><Link className="link-text" to="/contact-us">Lets Talk Business</Link></button>
-      </div>
-      <div
-        className="nav-right-option"
-        onClick={() =>
-          setRightMenu({ ...rightMenu, isOpen: !rightMenu.isOpen })
-        }
-      >
-        <span>{rightMenu.option}</span>{" "}
-        <span>
-          <FontAwesomeIcon
-            icon={faAngleDown}
-            className={`caret-icon ${rightMenu.isOpen ? "rotate" : ""}`}
-          />
-        </span>
+
+        <div className="buttons-container">
+          <button className="nav-button">
+            <Link className="link-text" to="/contact-us">
+              Lets Talk Business
+            </Link>
+          </button>
+        </div>
+
+        <div
+          className="nav-right-option"
+          onClick={() => setRightMenu({ ...rightMenu, isOpen: !rightMenu.isOpen })}
+        >
+          <span>{rightMenu.option}</span>{" "}
+          <span>
+            <FontAwesomeIcon
+              icon={faAngleDown}
+              className={`caret-icon ${rightMenu.isOpen ? "rotate" : ""}`}
+            />
+          </span>
+        </div>
       </div>
     </nav>
   );
