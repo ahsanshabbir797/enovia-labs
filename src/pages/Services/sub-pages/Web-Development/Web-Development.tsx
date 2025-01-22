cb5947e06f3ca4a4d00d502ac4d862080c0c16bb
ahsanshabbir797 <ahsanshabbir797@gmail.com>
2025-01-23 01:23:33 +0500

    created service cards for web development

--- src/pages/Services/sub-pages/Web-Development/Web-Development.tsx
+++ src/pages/Services/sub-pages/Web-Development/Web-Development.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import webDevelopmentCover from "../../../../assets/web-development.jpg";
 import styles from "./web-development.module.css";
 import ImageBanner from "../../../../components/ImageCover/ImageCover";
+import ServiceOverview from "../../../../components/ServiceOverview/ServiceOverview";
+import ServiceCards from "../../../../components/Service-Cards/ServiceCards";
 
 const WebDevelopment: React.FC = () => {
   return (
@@ -13,6 +15,10 @@ const WebDevelopment: React.FC = () => {
           bannerSubHeading="Building Seamless Experiences"
           height="48rem"
         />
+      </div>
+      <div className={styles["main-body-container"]}>
+        <ServiceOverview />
+        <ServiceCards />
       </div>
     </div>
   );
