73fb44e6227e2430a421f4c65e0707de7bf6c4bd
Malik Sharoz Tariq <maliksharoz70@outlook.com>
2025-01-23 14:43:40 +0500

    implement mobile development page

--- src/pages/Services/sub-pages/Mobile-Development/Mobile-Development.tsx
+++ src/pages/Services/sub-pages/Mobile-Development/Mobile-Development.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import styles from './mobile-development.module.css';
+import React from "react";
+import styles from "./mobile-development.module.css";
+import ImageBanner from "../../../../components/ImageCover/ImageCover";
+import mobiledevmain from "../../../../assets/mobiledevmain.jpg";
+import ServiceOverview from "../../../../components/ServiceOverview/ServiceOverview";
+import cardsData from "../../../../data.json";
+import overviewImage from "../../../../assets/mobiledevoverview.jpg";
+import ServiceCard from "../../../../components/Service-Card/ServiceCards";
 
 const MobileDevelopment: React.FC = () => {
-    return (
-        <div className={styles.container}>
-            <h1>Mobile Development</h1>
-            <p>Welcome to the Mobile Development page!</p>
-        </div>
-    );
+  const { servicesCards, servicesCardsHeading, servicesCardsContent } =
+    cardsData.ServicesPage.mobileDevelopment;
+  const { overviewText, subHeading, subHeadingContent } = servicesCardsContent;
+  return (
+    <div className={styles.container}>
+      <div className={styles.coverImage}>
+        <ImageBanner
+          imagePath={mobiledevmain}
+          bannerTitle="MOBILE APP DEVELOPMENT"
+          bannerSubHeading="Innovative Apps, Seamless Experiences, Limitless Possibilities"
+          height="48rem"
+        />
+      </div>
+      <div className={styles["main-body-container"]}>
+        <ServiceOverview
+          overviewText={overviewText}
+          subHeading={subHeading}
+          subHeadingContent={subHeadingContent}
+          overviewImage={overviewImage}
+        />
+        <ServiceCard
+          servicesCards={servicesCards}
+          servicesCardsHeading={servicesCardsHeading}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MobileDevelopment;
